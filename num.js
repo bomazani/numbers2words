4cecdae4030721236918c42a24251032c7601698
Bob Hunt <bob.hunt.email@gmail.com>
2018-02-07 15:20:58 -0500

    submitt

---
+++ num.js
@@ -0,0 +1,26 @@
+function numberToWord(){
+    //alert("hello");
+    var ones=["","one","two","three","four","five","six","seven", "eight", "nine","ten","eleven","twelve", "thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+    var tens=["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+    var hundreds=["","one hundred","two hundred","three hundred", "four hundred","five hundred","six hundred","seven hundred","eight hundred","nine hundred"]
+    for(i=0; i<=20; i++){
+        console.log(ones[i]);
+    }
+}
+  //  var onespos=0
+  //  var tens("")
+   // var tenspos=0
+  //  var hundreds("one hundred ","two hundred ", "three hundred ", "four hundred ", "five hundred ", "six hundred ", "seven hundred ", "eight hundred ", "nine hundred ")
+// for(let i=0, i<=9, i++)
+//     if(i=)
+
+// }
+// var newElement = document.createElement("div");
+//          // Place a text label inside the new div.
+//         var newText = document.createTextNode((j+1) +":"+ rollCount[j]);
+//         newElement.appendChild(newText);
+
+//         // Put the new div on the page inside the existing element "d1".
+//         var destination = document.getElementById("dice");
+//         destination.appendChild(newElement);
+//     }
